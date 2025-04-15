a2bc7584c4ea98677250bdcd4e54a58ec36ec0c7
sonPD <sonpd@vitalify.asia>
2025-04-15 15:21:11 +0700

    update: fix colors

--- bim/src/bim-loader.js
+++ bim/src/bim-loader.js
@@ -5,6 +5,18 @@ import * as WEBIFC from "web-ifc";
 
 export function setupBIMLoader(map) {
 
+    let classifier;
+    let color = new THREE.Color();
+    let slabs;
+    let walls;
+    let curtainWalls;
+    let furniture;
+    let doors;
+    let all;
+
+    let defaultWallColorCode = "#ebd6e7";
+    let defaultSlabsColorCode = "#ade1e5";
+
     map.addLayer({
         id: 'flight-route-layer',
         type: 'custom',
@@ -39,10 +51,9 @@ export function setupBIMLoader(map) {
 
         const fragments = components.get(OBC.FragmentsManager);
         const fragmentIfcLoader = components.get(OBC.IfcLoader);
+        classifier = components.get(OBC.Classifier);
 
         await fragmentIfcLoader.setup();
-
-        console.log("++++++");
 
         fragmentIfcLoader.settings.webIfc.COORDINATE_TO_ORIGIN = true;
 
@@ -65,16 +76,84 @@ export function setupBIMLoader(map) {
             model.rotation.set(Math.PI / 2, 0, 0);
             tb.add(model);
 
-            var threeMaterial = tb.material({
-                material: new THREE.MeshStandardMaterial({ color: '#adfc03' }),
+            classifier.byEntity(model);
+
+            slabs = classifier.find({
+                entities: ["IFCSLAB"],
             });
 
+            walls = classifier.find({
+                entities: ["IFCWALLSTANDARDCASE"],
+            });
 
+            curtainWalls = classifier.find({
+                entities: ["IFCMEMBER", "IFCPLATE"],
+            });
+
+            furniture = classifier.find({
+                entities: ["IFCFURNISHINGELEMENT"],
+            });
+
+            doors = classifier.find({
+                entities: ["IFCDOOR"],
+            });
+
+            all = classifier.find({
+                models: [model.uuid],
+            });
+
+            classifier.setColor(walls, new THREE.Color(defaultWallColorCode));
+            classifier.setColor(slabs, new THREE.Color(defaultSlabsColorCode));
+
+            LoadUI();
         }
 
         //fragments.onFragmentsLoaded.add((model) => {
         //    console.log(model);
         //});
     }
+
+    function LoadUI() {
+        BUI.Manager.init();
+
+        const panel = BUI.Component.create(() => {
+            return BUI.html`
+    <bim-panel active label="Set Color" class="options-menu">
+      <bim-panel-section collapsed label="Controls">
+      
+        <bim-color-input 
+          label="Walls Color" color="#ebd6e7" id="wcolorbutton"
+          @input="${({ target }) => {
+                    color.set(target.color)
+                    classifier.setColor(walls, color)
+                }}">
+        </bim-color-input>
+      
+        <bim-color-input 
+          label="Slabs Color" color="#ade1e5" id="slabcolorbutton"
+          @input="${({ target }) => {
+                    color.set(target.color)
+                    classifier.setColor(slabs, color)
+                }}">
+        </bim-color-input>
+
+        <bim-button 
+          label="Reset walls color" 
+          @click="${() => {
+                document.getElementById("wcolorbutton").color = defaultWallColorCode;
+                document.getElementById("slabcolorbutton").color = defaultSlabsColorCode;
+                classifier.setColor(walls, new THREE.Color(defaultWallColorCode));
+                classifier.setColor(slabs, new THREE.Color(defaultSlabsColorCode));
+                }}">  
+        </bim-button>  
+
+      </bim-panel-section>
+    </bim-panel>
+    `
+        })
+
+        document.body.append(panel);
+    }
+
 }
 
